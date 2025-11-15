5f830820964618ebc135b1c0e5bf5e1dc40103df
fireattack <human.peng@gmail.com>
2025-11-15 19:48:05 +0800

    update graphql url and parameter for BroadcastQuery

--- TwitterSpace.py
+++ TwitterSpace.py
@@ -102,31 +102,41 @@ class TwitterSpace:
             }
             features = {
                 "creator_subscriptions_tweet_preview_api_enabled": True,
+                "premium_content_api_read_enabled": False,
                 "communities_web_enable_tweet_community_results_fetch": True,
                 "c9s_tweet_anatomy_moderator_badge_enabled": True,
+                "responsive_web_grok_analyze_button_fetch_trends_enabled": False,
+                "responsive_web_grok_analyze_post_followups_enabled": True,
+                "responsive_web_jetfuel_frame": True,
+                "responsive_web_grok_share_attachment_enabled": True,
                 "articles_preview_enabled": True,
+                "payments_enabled": False,
+                "profile_label_improvements_pcf_label_in_post_enabled": True,
+                "responsive_web_profile_redirect_enabled": False,
                 "rweb_tipjar_consumption_enabled": True,
-                "responsive_web_graphql_exclude_directive_enabled": True,
                 "verified_phone_label_enabled": False,
-                "tweetypie_unmention_optimization_enabled": True,
                 "responsive_web_edit_tweet_api_enabled": True,
                 "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
                 "view_counts_everywhere_api_enabled": True,
                 "longform_notetweets_consumption_enabled": True,
                 "responsive_web_twitter_article_tweet_consumption_enabled": True,
                 "tweet_awards_web_tipping_enabled": False,
+                "responsive_web_grok_show_grok_translated_post": False,
+                "responsive_web_grok_analysis_button_from_backend": True,
                 "creator_subscriptions_quote_tweet_preview_enabled": False,
                 "freedom_of_speech_not_reach_fetch_enabled": True,
                 "standardized_nudges_misinfo": True,
                 "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": True,
-                "rweb_video_timestamps_enabled": True,
                 "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
                 "longform_notetweets_rich_text_read_enabled": True,
                 "longform_notetweets_inline_media_enabled": True,
+                "responsive_web_grok_image_annotation_enabled": True,
+                "responsive_web_grok_imagine_annotation_enabled": True,
+                "responsive_web_grok_community_note_auto_translation_is_enabled": False,
                 "responsive_web_graphql_timeline_navigation_enabled": True,
                 "responsive_web_enhance_cards_enabled": False
             }
-            url = "https://x.com/i/api/graphql/Ft426awxxM1206ZcaShbDw/BroadcastQuery"
+            url = "https://x.com/i/api/graphql/gMKnsAOgD-xxxUgDkJFyAg/BroadcastQuery"
 
         variables_str = json.dumps(variables)
         features_str = json.dumps(features)
@@ -355,18 +365,21 @@ class TwitterSpace:
 
         :raises AssertionError: If both guest_token and cookies are None.
         """
+        USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
         assert guest_token is not None or cookies is not None, 'guest_token or cookies must be provided.'
         if guest_token:
             self.session.headers.update({
                 "authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
-                "x-guest-token": guest_token
+                "x-guest-token": guest_token,
+                'user-agent': USER_AGENT,
             })
         else:
             cookie_header = "; ".join([f"{name}={value}" for name, value in cookies.items()])
             self.session.headers.update({
                 "authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
                 "x-csrf-token": cookies['ct0'],
-                "cookie": cookie_header
+                "cookie": cookie_header,
+                'user-agent': USER_AGENT,
             })
 
     def generate_filename(self):
