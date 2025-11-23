7911b2cd393ab2fc93bd873063e8f96d9f3e0ed8
fireattack <human.peng@gmail.com>
2025-11-24 00:40:47 +0800

    Update AudioSpaceById URL and params

--- TwitterSpace.py
+++ TwitterSpace.py
@@ -80,23 +80,47 @@ class TwitterSpace:
             variables = {
                 "id": space_id,
                 "isMetatagsQuery": True,
-                "withSuperFollowsUserFields": True,
-                "withDownvotePerspective": False,
-                "withReactionsMetadata": False,
-                "withReactionsPerspective": False,
-                "withSuperFollowsTweetFields": True,
-                "withReplays": True
+                "withReplays": True,
+                "withListeners": True
             }
             features = {
-                "dont_mention_me_view_api_enabled": True,
-                "interactive_text_enabled": True,
-                "responsive_web_uc_gql_enabled": False,
-                "vibe_tweet_context_enabled": False,
-                "responsive_web_edit_tweet_api_enabled": False,
-                "standardized_nudges_for_misinfo_nudges_enabled": False
+                "spaces_2022_h2_spaces_communities": True,
+                "spaces_2022_h2_clipping": True,
+                "creator_subscriptions_tweet_preview_api_enabled": True,
+                "profile_label_improvements_pcf_label_in_post_enabled": True,
+                "responsive_web_profile_redirect_enabled": False,
+                "rweb_tipjar_consumption_enabled": True,
+                "verified_phone_label_enabled": False,
+                "premium_content_api_read_enabled": False,
+                "communities_web_enable_tweet_community_results_fetch": True,
+                "c9s_tweet_anatomy_moderator_badge_enabled": True,
+                "responsive_web_grok_analyze_button_fetch_trends_enabled": False,
+                "responsive_web_grok_analyze_post_followups_enabled": True,
+                "responsive_web_jetfuel_frame": True,
+                "responsive_web_grok_share_attachment_enabled": True,
+                "articles_preview_enabled": True,
+                "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
+                "responsive_web_edit_tweet_api_enabled": True,
+                "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
+                "view_counts_everywhere_api_enabled": True,
+                "longform_notetweets_consumption_enabled": True,
+                "responsive_web_twitter_article_tweet_consumption_enabled": True,
+                "tweet_awards_web_tipping_enabled": False,
+                "responsive_web_grok_show_grok_translated_post": False,
+                "responsive_web_grok_analysis_button_from_backend": True,
+                "creator_subscriptions_quote_tweet_preview_enabled": False,
+                "freedom_of_speech_not_reach_fetch_enabled": True,
+                "standardized_nudges_misinfo": True,
+                "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": True,
+                "longform_notetweets_rich_text_read_enabled": True,
+                "longform_notetweets_inline_media_enabled": True,
+                "responsive_web_grok_image_annotation_enabled": True,
+                "responsive_web_grok_imagine_annotation_enabled": True,
+                "responsive_web_graphql_timeline_navigation_enabled": True,
+                "responsive_web_grok_community_note_auto_translation_is_enabled": False,
+                "responsive_web_enhance_cards_enabled": False
             }
-            #TODO: update to latest graphql url when possible
-            url = "https://twitter.com/i/api/graphql/yMLYE2ltn1nOZ5Gyk3JYSw/AudioSpaceById"
+            url = "https://twitter.com/i/api/graphql/rC2zlE1t7SHbVG8obPZliQ/AudioSpaceById"
         else:
             variables = {
                 "id": space_id
